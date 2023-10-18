1f1a8fff8a7e4991a3513fbaebd07ad0f9e661c9
sancheznot <sancheznotp@gmail.com>
2023-10-18 04:18:10 -0700

    delete padding of calcullators button

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="w-full h-screen">{children}</body>
+      <body className="w-full ">{children}</body>
     </html>
   )
 }
